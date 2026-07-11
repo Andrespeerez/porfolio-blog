0c3634e7253ef8bb8fa4e74ad4dc8dd0f872b780
Andrespeerez <andreslavila93@gmail.com>
2026-07-11 13:49:39 +0200

    refactor: add pagination calculation to DTO

--- Application/DTOs/Common/PagedResult.cs
+++ Application/DTOs/Common/PagedResult.cs
@@ -1,9 +1,12 @@
 namespace Application.DTOs.Common;
 
 public record PagedResult<T>(
-    IReadOnlyList<T> Items,
+    IReadOnlyList<T>? Items,
     int Page,
     int PageSize,
     int TotalCount
-);
+)
+{
+    public int TotalPages => PageSize > 0 ? (TotalCount + PageSize - 1) / PageSize : 0;
+}
 
